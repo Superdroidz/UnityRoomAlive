9f043ac32ba33030ea26de7e47bdc1efabb8dbcf
Adam Gestwa <adam.gestwa13@imperial.ac.uk>
2015-11-26 00:03:25 +0000

    Finished basic implementation with stub methods

--- Assets/Scripts/Editor/Servers/PrimaryServerWindow.cs
+++ Assets/Scripts/Editor/Servers/PrimaryServerWindow.cs
@@ -5,13 +5,35 @@ using System.Collections;
 public class PrimaryServerWindow : EditorWindow {
 	
 	private static string windowTitle = "Primary Server";
+	private static int buttonWidth = 130;
+	
+	string localKinects = "0";
+	string localProjectors = "0";
 
-	string myString = "Hello World";
-	bool groupEnabled;
-	bool myBool = true;
-	float myFloat = 1.23f;
+	string totalKinects = "0";
+	string totalProjectors = "0";
 
 	public PrimaryServerWindow(){
+	}
+
+	private static void StartProjectorServers()
+	{
+
+	}
+
+	private static void StartKinectServers()
+	{
+
+	}
+
+	private static void FindKinects()
+	{
+
+	}
+
+	private static void FindProjectors()
+	{
+
 	}
 
 	public void ShowWindow()
@@ -24,12 +46,40 @@ public class PrimaryServerWindow : EditorWindow {
 	}
 	
 	void OnGUI () {
-		GUILayout.Label ("Base Settings", EditorStyles.boldLabel);
-		myString = EditorGUILayout.TextField ("Text Field", myString);
-		
-		groupEnabled = EditorGUILayout.BeginToggleGroup ("Optional Settings", groupEnabled);
-		myBool = EditorGUILayout.Toggle ("Toggle", myBool);
-		myFloat = EditorGUILayout.Slider ("Slider", myFloat, -3, 3);
-		EditorGUILayout.EndToggleGroup ();
+		//Local Kinect Section
+		EditorGUILayout.BeginHorizontal();
+		EditorGUILayout.TextField ("Local Kinects: ", localKinects);
+		if (GUILayout.Button("Start Kinect Server",GUILayout.Width(buttonWidth)))
+		{
+			StartKinectServers();
+		};
+		EditorGUILayout.EndHorizontal();
+
+		//Local Projectors Section
+		EditorGUILayout.BeginHorizontal();
+		EditorGUILayout.TextField ("Local Projectors: ", localProjectors);
+		if (GUILayout.Button("Start Projector Server",GUILayout.Width(buttonWidth)))
+		{
+			StartProjectorServers();
+		}
+		EditorGUILayout.EndHorizontal();
+
+		//Total Kinect Section
+		EditorGUILayout.BeginHorizontal();
+		EditorGUILayout.LabelField("Total Number of Kinects: ", totalKinects);
+		if (GUILayout.Button("Find Kinects",GUILayout.Width(buttonWidth)))
+		{
+			FindKinects();
+		}
+		EditorGUILayout.EndHorizontal();
+
+		//Total Projectors Section
+		EditorGUILayout.BeginHorizontal();
+		EditorGUILayout.LabelField("Total Number of Projectors: ", totalProjectors);
+		if (GUILayout.Button("Find Projectors", GUILayout.Width(buttonWidth)))
+		{
+			FindProjectors();
+		}
+		EditorGUILayout.EndHorizontal();
 	}
 }
