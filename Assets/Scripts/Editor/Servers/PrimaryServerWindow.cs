58fd7b4e8bf32fc38385caa66332f12ca7444656
Anton Alenov <aa16212@ic.ac.uk>
2016-01-05 19:37:36 +0000

    Add server start/stop functionality
    It is now possible to start and stop kinect and projector servers from
    inside the Unity editor.

--- Assets/Scripts/Editor/Servers/PrimaryServerWindow.cs
+++ Assets/Scripts/Editor/Servers/PrimaryServerWindow.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEditor;
-using System.Collections;
+using System.Diagnostics;
+using System;
 
 public class PrimaryServerWindow : EditorWindow {
 	
@@ -13,20 +14,69 @@ public class PrimaryServerWindow : EditorWindow {
 	string totalKinects = "0";
 	string totalProjectors = "0";
 
-	public PrimaryServerWindow(){
+    // local server process holders
+    static Process projectorServer;
+    static Process kinectServer;
+
+    // TODO: should be relative, not absolute
+    static private string projectorServerPath = @"C:\foxbox\dev\RoomAliveTK\ProCamCalibration\ProjectorServer\bin\Debug\ProjectorServer.exe";
+    static private string kinectServerPath = @"";
+
+    // begins a process from path with arguments and stores it in out
+    private static void StartProcess(out Process proc, string processPath, string args)
+    {
+        ProcessStartInfo startInfo = new ProcessStartInfo();
+        startInfo.FileName = processPath;
+        startInfo.Arguments = args;
+        //startInfo.RedirectStandardOutput = false;
+        //startInfo.RedirectStandardError = false;
+        //startInfo.UseShellExecute = false;
+        //startInfo.CreateNoWindow = true;
+        startInfo.WindowStyle = ProcessWindowStyle.Minimized;
+
+        proc = new Process();
+        proc.StartInfo = startInfo;
+        proc.EnableRaisingEvents = true;
+        try
+        {
+            proc.Start();
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+
+    public PrimaryServerWindow(){
 	}
 
 	private static void StartProjectorServers()
 	{
-
+        StartProcess(out projectorServer, projectorServerPath, "");
 	}
+
+    private static void KillProjectorServer()
+    {
+        if (projectorServer != null && !projectorServer.HasExited)
+        {
+            projectorServer.Kill();
+        }
+    }
 
 	private static void StartKinectServers()
 	{
+        StartProcess(out kinectServer, projectorServerPath, "");
+    }
 
-	}
+    private static void KillKinectServers()
+    {
+        if (kinectServer != null && !kinectServer.HasExited)
+        {
+            kinectServer.Kill();
+        }
+    }
 
-	private static void FindKinects()
+    private static void FindKinects()
 	{
 
 	}
@@ -52,17 +102,25 @@ public class PrimaryServerWindow : EditorWindow {
 		if (GUILayout.Button("Start Kinect Server",GUILayout.Width(buttonWidth)))
 		{
 			StartKinectServers();
-		};
-		EditorGUILayout.EndHorizontal();
+		}
+        if (GUILayout.Button("Stop Kinect Server", GUILayout.Width(buttonWidth)))
+        {
+            KillKinectServers();
+        };
+        EditorGUILayout.EndHorizontal();
 
 		//Local Projectors Section
 		EditorGUILayout.BeginHorizontal();
 		localProjectors = EditorGUILayout.TextField ("Local Projectors: ", localProjectors);
-		if (GUILayout.Button("Start Projector Server",GUILayout.Width(buttonWidth)))
+		if (GUILayout.Button("Start Server",GUILayout.Width(buttonWidth)))
 		{
 			StartProjectorServers();
 		}
-		EditorGUILayout.EndHorizontal();
+        if (GUILayout.Button("Stop Server", GUILayout.Width(buttonWidth)))
+        {
+            KillProjectorServer();
+        }
+        EditorGUILayout.EndHorizontal();
 
 		//Total Kinect Section
 		EditorGUILayout.BeginHorizontal();
