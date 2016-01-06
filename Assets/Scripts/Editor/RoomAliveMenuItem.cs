2f6e918342ab98b95122e7e314f8ff69aa86ec80
Adam Gestwa <Adam Gestwa>
2016-01-06 18:41:43 +0000

    Finished Basic RoomAlive ToolBar

--- Assets/Scripts/Editor/RoomAliveMenuItem.cs
+++ Assets/Scripts/Editor/RoomAliveMenuItem.cs
@@ -1,45 +1,58 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using System.Collections;
+using System.Diagnostics;
 
 public class RoomAliveMenuItem : EditorWindow{
+    private static bool fileSetupComplete = false;
+    private static bool calibrationComplete = false;
 
-	public static PrimaryServerWindow PrimaryWindow;
-	public static SecondaryServerWindow SecondaryWindow;
-	//public static SlaveServerWindow window;
+    [MenuItem("RoomAlive/Start Kinect Server", false, 1)]
+    private static void RunKinectServer()
+    {
+        System.Diagnostics.Process.Start("C:\\Users\\Adam\\Desktop\\3rdYearProject\\UnityExtension\\RoomAlive\\RoomAliveToolkit-master\\ProCamCalibration\\KinectServer\\bin\\Debug\\KinectServer.exe");
+    }
 
-	[MenuItem("RoomAlive/Start Server")]
-	
-	[MenuItem("RoomAlive/Start Server/Primary", false, 1)] // Should require Validation
-	private static void OpenPrimaryServer()
-	{
-		PrimaryWindow = (PrimaryServerWindow)ScriptableObject.CreateInstance("PrimaryServerWindow");
-		PrimaryWindow.ShowWindow();
+    [MenuItem("RoomAlive/Start Projector Server", false, 2)]
+    private static void RunProjectorServer()
+    {
+        System.Diagnostics.Process.Start("C:\\Users\\Adam\\Desktop\\3rdYearProject\\UnityExtension\\RoomAlive\\RoomAliveToolkit-master\\ProCamCalibration\\ProjectorServer\\bin\\Debug\\ProjectorServer.exe");
 
-	}
+    }
 
-	[MenuItem("RoomAlive/Start Server/Secondary", false, 2)] // Should require Validation
-	private static void OpenSecondaryServer()
-	{
-		SecondaryWindow = (SecondaryServerWindow)ScriptableObject.CreateInstance("SecondaryServerWindow");
-		SecondaryWindow.ShowWindow();
-	}
+    [MenuItem("RoomAlive/Create New Setup", false, 51)]
+    private static void CreateSetup()
+    {
+        fileSetupComplete = false;
+        calibrationComplete = false;
+        //Discover other Kinect Servers and Projector Servers
+        //Create XML file with modified ip address, names and display indexes.
+        fileSetupComplete = true;
+    }
 
-	[MenuItem("RoomAlive/Acquire", false, 51)] // Should require Validation
-	private static void Acquire()
-	{
+    [MenuItem("RoomAlive/Run Calibration", false, 101)]// Requires Validation
+    private static void Calibrate()
+    {
+        //Run Acquire
+        //Run Solve
+        calibrationComplete = true;
+    }
 
-	}
+    [MenuItem("RoomAlive/Run Calibration", true)]
+    private static bool CalibrationValidation()
+    {
+        return fileSetupComplete;
+    }
+    
+    [MenuItem("RoomAlive/Import Room",false, 151)]
+    private static void ImportRoom()
+    {
+        EditorApplication.ExecuteMenuItem("Assets/Import New Asset...");
+    }
 
-	[MenuItem("RoomAlive/Solve", false, 52)] // Should require Validation
-	private static void Solve()
-	{
-
-	}
-
-	[MenuItem("RoomAlive/Import Room", false, 101)] // Should require Validation
-	private static void ImportRoom()
-	{
-
-	}
+    [MenuItem("RoomAlive/Import Room", true)]
+    private static bool ImportRoomValidation()
+    {
+        return calibrationComplete;
+    }
 }
