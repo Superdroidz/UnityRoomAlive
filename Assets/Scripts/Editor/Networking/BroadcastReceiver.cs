dcb03ec5485c89c31118b92c146604d35662cdcf
Anton Alenov <aa16212@ic.ac.uk>
2016-01-07 18:47:34 +0000

    Add simple networking discovery via UDP boradcast

---
+++ Assets/Scripts/Editor/Networking/BroadcastReceiver.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+[ExecuteInEditMode]
+public class BroadcastReceiver {
+    UdpClient receiver;
+
+    public void ListenForBroadcast()
+    {
+        try
+        {
+            if (receiver == null)
+            {
+                receiver = new UdpClient(PortSetup.listenerPort);
+                receiver.BeginReceive(new AsyncCallback(ReceiveData), null);
+            }
+        }
+        catch (SocketException e)
+        {
+            Debug.Log(e.Message);
+        }
+    }
+
+    void ReceiveData(IAsyncResult result)
+    {
+        IPEndPoint receiveIPGroup = new IPEndPoint(IPAddress.Any, PortSetup.listenerPort);
+        byte[] received;
+        if (receiver != null)
+        {
+            received = receiver.EndReceive(result, ref receiveIPGroup);
+            Debug.Log("Got ip: " + Encoding.ASCII.GetString(received));
+        }
+    }
+}
---
+++ Assets/Scripts/Editor/Networking/PortSetup.cs
@@ -0,0 +1,4 @@
+﻿public class PortSetup {
+    public static int listenerPort = 12893;
+    public static int broadcasterPort = 12894;
+}
