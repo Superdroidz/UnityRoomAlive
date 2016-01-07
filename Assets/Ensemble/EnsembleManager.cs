d5df58c70ec6c6e4986ce6775365548ca2fee85b
Louis Chan <louis058@gmail.com>
2016-01-07 13:21:11 +0000

    Add method to debug camera corners

--- Assets/Ensemble/EnsembleManager.cs
+++ Assets/Ensemble/EnsembleManager.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Ensemble {
@@ -21,28 +23,60 @@ namespace Ensemble {
         void SetCameraFromProjector(ProjectorData projector) {
             // Scale z by -1 because Unity uses OpenGL convention where Camera's forward
             // direction is negative.
-            Matrix4x4 worldToCameraMatrix = Matrix4x4.Scale(new Vector3(1, 1, -1)) * projector.pose.inverse;
-            // Scale down translations by 10 (seems to help in the case of 219b's calibration)
-            for (int i = 0; i <= 2; i++) {
-                worldToCameraMatrix[i, 3] = worldToCameraMatrix[i, 3] / 10;
-            }
+            Matrix4x4 worldToCameraMatrix = projector.pose.inverse;
+            worldToCameraMatrix[2, 2] = -worldToCameraMatrix[2, 2];
+            //for (int i = 0; i <= 2; i++) {
+            //    worldToCameraMatrix[i, 3] /= 10;
+            //}
             Debug.Log(worldToCameraMatrix);
             Camera.main.worldToCameraMatrix = worldToCameraMatrix;
-            Camera.main.projectionMatrix =
-                ProjectionMatrixFromCameraMatrix(projector.cameraMatrix, projector.width, projector.height);
 
-            Camera.main.tag = "MainCamera";
+            Matrix4x4 projectionMatrix = ProjectionMatrixFromCameraMatrix(projector.cameraMatrix, projector.width, projector.height);
+            Debug.Log(projectionMatrix);
+            Camera.main.projectionMatrix = projectionMatrix;
+
+            DebugViewCameraCorners();
+
+            //Matrix4x4 rotationMatrix = projector.pose.inverse;
+            //Vector4 translation_vec4 = rotationMatrix.GetColumn(3);
+            //Vector3 translation = new Vector3(translation_vec4.x, translation_vec4.y, translation_vec4.z);
+            //rotationMatrix.SetColumn(3, new Vector4(0, 0, 0, 1));
+            //Vector3 scale = Vector3.one;
+            //for (int j = 0; j < 3; j++) {
+            //    scale[j] = rotationMatrix.GetColumn(j).magnitude;
+            //    rotationMatrix.SetColumn(j, rotationMatrix.GetColumn(j) / scale[j]);
+            //}
+            //Quaternion rotation = QuaternionFromMatrix(rotationMatrix);
+            //Camera.main.transform.Translate(translation);
+            //Camera.main.transform.rotation = rotation;
+            //Camera.main.transform.localScale = scale;
         }
 
-        Matrix4x4 ProjectionMatrixFromCameraMatrix(Matrix4x4 cameraMatrix,
+        private void DebugViewCameraCorners() {
+            List<Ray> rays = new List<Ray> {
+                Camera.main.ViewportPointToRay(new Vector3(0, 0, 0)),
+                Camera.main.ViewportPointToRay(new Vector3(1, 0, 0)),
+                Camera.main.ViewportPointToRay(new Vector3(1, 1, 0)),
+                Camera.main.ViewportPointToRay(new Vector3(0, 1, 0))
+            };
+
+            RaycastHit hit;
+            foreach (Ray ray in rays) {
+                if (Physics.Raycast(ray, out hit, 100)) {
+                    Gizmos.DrawCube(hit.transform.position, Vector3.one / 3);
+                }
+            }
+        }
+
+        public static Matrix4x4 ProjectionMatrixFromCameraMatrix(Matrix4x4 cameraMatrix,
                 float projectorWidth, float projectorHeight) {
             float fx = cameraMatrix[0, 0];
             float fy = cameraMatrix[1, 1];
             float cx = cameraMatrix[0, 2];
             float cy = cameraMatrix[1, 2];
 
-            float near = 0.3f;
-            float far = 1000.0f;
+            float near = 0.1f;
+            float far = 100.0f;
 
             float w = projectorWidth;
             float h = projectorHeight;
@@ -53,6 +87,19 @@ namespace Ensemble {
                 m20 = 0, m21 = 0, m22 = far / (far - near), m23 = -near * far / (far - near),
                 m30 = 0, m31 = 0, m32 = 1, m33 = 0
             };
+        }
+
+        public static Quaternion QuaternionFromMatrix(Matrix4x4 m) {
+            // Adapted from: http://www.euclideanspace.com/maths/geometry/rotations/conversions/matrixToQuaternion/index.htm
+            Quaternion q = new Quaternion();
+            q.w = Mathf.Sqrt(Mathf.Max(0, 1 + m[0, 0] + m[1, 1] + m[2, 2])) / 2;
+            q.x = Mathf.Sqrt(Mathf.Max(0, 1 + m[0, 0] - m[1, 1] - m[2, 2])) / 2;
+            q.y = Mathf.Sqrt(Mathf.Max(0, 1 - m[0, 0] + m[1, 1] - m[2, 2])) / 2;
+            q.z = Mathf.Sqrt(Mathf.Max(0, 1 - m[0, 0] - m[1, 1] + m[2, 2])) / 2;
+            q.x *= Mathf.Sign(q.x * (m[2, 1] - m[1, 2]));
+            q.y *= Mathf.Sign(q.y * (m[0, 2] - m[2, 0]));
+            q.z *= Mathf.Sign(q.z * (m[1, 0] - m[0, 1]));
+            return q;
         }
     }
 }
