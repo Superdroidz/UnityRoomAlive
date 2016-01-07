0344239bed96ba7c10a1485e51ad08bbb60934f7
Anton Alenov <aa16212@ic.ac.uk>
2016-01-07 13:11:01 +0000

    Add editor menu+shortcut to recursively add mesh colliders to object
    Select GameObject in need of mesh collision in the hierarchy and press
    ctrl+m.

--- Assets/Scripts/Editor/RoomAliveMenuItem.cs
+++ Assets/Scripts/Editor/RoomAliveMenuItem.cs
@@ -2,12 +2,39 @@
 using UnityEditor;
 using System.Collections;
 using System.Diagnostics;
+using System;
 
 public class RoomAliveMenuItem : EditorWindow{
     public static ParseWindow ParseWindow;
 
     private static bool fileSetupComplete = false;
     private static bool calibrationComplete = false;
+
+    [MenuItem("Custom Editor/Add Mesh Colliders %m")]
+    private static void PopulateMeshColliders()
+    {
+        GameObject selection = Selection.activeGameObject;
+        AddMeshToObjectOrChildren(selection);
+    }
+
+    private static void AddMeshToObjectOrChildren(GameObject selection)
+    {
+        if (selection.GetComponent<MeshFilter>() != null &&
+            selection.GetComponent<Collider>() == null)
+        {
+            var filter = selection.GetComponent<MeshFilter>().sharedMesh;
+            MeshCollider collider = selection.AddComponent<MeshCollider>();
+            collider.sharedMesh = filter;
+            UnityEngine.Debug.Log("Added MeshCollider to object " + selection.name);
+        }
+        else
+        {
+            foreach (Transform child in selection.transform)
+            {
+                AddMeshToObjectOrChildren(child.gameObject);
+            }
+        }
+    }
 
     [MenuItem("RoomAlive/Start Kinect Server", false, 1)]
     private static void RunKinectServer()
