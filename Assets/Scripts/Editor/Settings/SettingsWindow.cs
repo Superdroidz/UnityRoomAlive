4d9e6276453f0a0fd505e08bed09eaad4d9fcdd5
Adam Gestwa <Adam Gestwa>
2016-01-13 18:16:28 +0000

    Saves settings paths so they dont need to be re-entered on unity startup

--- Assets/Scripts/Editor/Settings/SettingsWindow.cs
+++ Assets/Scripts/Editor/Settings/SettingsWindow.cs
@@ -7,11 +7,27 @@ using System.IO;
 
 public class SettingsWindow : EditorWindow {
 
+    class SettingsData
+    {
+        private string kinectServerPath;
+        private string projectorServerPath;
+        private string consoleApplicationPath;
+
+        public SettingsData(string kinectPath, string projectorPath, string consolePath)
+        {
+            kinectServerPath = kinectPath;
+            projectorServerPath = projectorPath;
+            consoleApplicationPath = consolePath;
+        }
+    }
+
     private static string windowTitle = "RoomAlive Settings";
     private static int buttonWidth = 130;
     private static string kinectServerPath;
     private static string projectorServerPath;
     private static string consoleApplicationPath;
+    private static string settingsFilePath = Application.dataPath + "/settings.xml";
+    private XmlDocument settings;
 
     public string KinectServerPath
     {
@@ -49,6 +65,23 @@ public class SettingsWindow : EditorWindow {
         set
         {
             consoleApplicationPath = value;
+        }
+    }
+
+    private void LoadSettings()
+    {
+        if (File.Exists(settingsFilePath))
+        {
+            settings = new XmlDocument();
+
+            settings.Load(settingsFilePath);
+
+            kinectServerPath = settings.GetElementsByTagName("KinectPath")[0].InnerText;
+            projectorServerPath = settings.GetElementsByTagName("ProjectorPath")[0].InnerText;
+            consoleApplicationPath = settings.GetElementsByTagName("ConsoleApplication")[0].InnerText;
+        }
+        else
+        {
         }
     }
 
@@ -90,6 +123,42 @@ public class SettingsWindow : EditorWindow {
         }
         EditorGUILayout.EndHorizontal();
 
+        //Saving settings to XML Button
+        EditorGUILayout.BeginHorizontal();
+        if (GUILayout.Button("Save Settings", GUILayout.Width(buttonWidth)))
+        {
+            SettingsData settings = new SettingsData(kinectServerPath, kinectServerPath, consoleApplicationPath);
+
+            XmlWriterSettings XMLsettings = new XmlWriterSettings();
+            XMLsettings.Indent = true;
+
+            using (FileStream fileStream = new FileStream(settingsFilePath, FileMode.Create))
+            using (StreamWriter sw = new StreamWriter(fileStream))
+            using (XmlTextWriter writer = new XmlTextWriter(sw))
+            {
+                writer.Formatting = Formatting.Indented;
+                writer.Indentation = 4;
+
+                UnityEngine.Debug.Log("Got to save the file");
+                writer.WriteStartDocument();
+                writer.WriteStartElement("Settings");
+
+                writer.WriteElementString("KinectPath", kinectServerPath);
+                writer.WriteElementString("ProjectorPath", projectorServerPath);
+                writer.WriteElementString("ConsoleApplication", consoleApplicationPath);
+
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+            }
+        }
+
+        //Loading settings from file
+        if (GUILayout.Button("Load Settings", GUILayout.Width(buttonWidth)))
+        {
+            LoadSettings();
+        }
+
+        EditorGUILayout.EndHorizontal();
     }
 
 }
