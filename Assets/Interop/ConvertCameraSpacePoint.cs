4644499b1f1a87f0f41c2844f7db4b4fc16c989f
Louis Chan <louis058@gmail.com>
2016-01-13 18:50:19 +0000

    Flip x point of CameraSpacePoint

--- Assets/Interop/ConvertCameraSpacePoint.cs
+++ Assets/Interop/ConvertCameraSpacePoint.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 namespace Assets.Interop {
     class ConvertCameraSpacePoint {
         public static Vector3 ToVector3(CameraSpacePoint cameraSpacePoint) {
-            return new Vector3(cameraSpacePoint.X, cameraSpacePoint.Y, cameraSpacePoint.Z);
+            return new Vector3(-cameraSpacePoint.X, cameraSpacePoint.Y, cameraSpacePoint.Z);
         }
     }
 }
